a90151015981afc3748fc93fbd808d95993cdead
Vanessa Madu <vm2218@ic.ac.uk>
2023-11-25 20:20:47 +0000

    add attributes of the CSV manager class

--- data_handling.py
+++ data_handling.py
@@ -1,3 +1,22 @@
 # DATA HANDLING LIBRARIES
 import pandas as pd ## CSV
 import h5py         ## HDF5
+
+# DATA MANAGER CLASSES
+
+class CSVManager:
+    def __init__(self,file_path,delimiter,has_header,read_only,
+                 error_handling,encoding,line_endings, datetime_format,
+                 archive_status):
+        self.file_path = file_path
+        self.delimiter = delimiter
+        self.has_header = has_header
+        self.column_names = None
+        self.data = None
+        self.read_only = read_only
+        self.error_handling = error_handling
+        self.encoding = encoding
+        self.line_endings = line_endings
+        self.metadata = {}
+        self.datetime_format = datetime_format
+        self.archive_status = archive_status
