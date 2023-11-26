8dabfa06a95167c75931c5c8a20b300599e978ac
Vanessa Madu <vm2218@ic.ac.uk>
2023-11-26 20:35:14 +0000

    add hdf5 read class

--- test_data_handling.py
+++ test_data_handling.py
@@ -1,6 +1,16 @@
 # Description: Test file for data_handling.py
 
-from data_handling import *
+from data_handling import HDF5Manager
 import pytest
+import h5py
 
 # HDF5Manager Tests
+class HDF5_Read_Tests:
+    # Setup test files
+    @pytest.fixture
+    def set_up_existing_file(self):
+        file_path = 'existing_file.h5'
+        with h5py.File(file_path,'w') as file:
+            group = file.create_group('/test_group')
+            group.create_dataset('test_dataset', data=[1,2,3])
+        return file_path
