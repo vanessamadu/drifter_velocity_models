13a97c20dd90c6127003c89a9bf60b204b651531
Vanessa Madu <vm2218@ic.ac.uk>
2023-12-04 12:32:59 +0000

    new column drifter speed

--- drifter_velocity_models/fixed_current_map.py
+++ drifter_velocity_models/fixed_current_map.py
@@ -2,5 +2,21 @@
 import pandas as pd
 import numpy as np
 
+# calculate drifter speed from data and add the corresponding column to the data
+def new_col_drifter_speed(u_array,v_array,data):
+    drifter_speed = np.array([u**2+v**2 for u,v in zip(u_array,v_array)])
+    data["Drifter Speed"] = drifter_speed
+
+
+
+
+
+
+
 # load data
 data = pd.read_hdf("ocean_data.h5")
+# add drifter speed column to data
+
+
+
+
