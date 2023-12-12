8031f6eaa465a1c6b803d7e5ad75005140dad351
Vanessa Madu <vm2218@ic.ac.uk>
2023-12-12 13:26:23 +0000

    set up

--- drifter_velocity_models/model_classes.py
+++ drifter_velocity_models/model_classes.py
@@ -1,4 +1,16 @@
 'description: hierarchical model framework for the construction of my ocean drifter velocity model'
 
+####################################### SET UP ##############################################
+##### import packages #####
+import numpy as np
+from numpy import linalg
+import pandas as pd
+import math
+from scipy import stats
+# plotting #
+import matplotlib.pyplot as plt
 
+##### load data #####
+data = pd.read_hdf("ocean_data.h5")
 
+####################################### MODEL CLASS ##########################################
