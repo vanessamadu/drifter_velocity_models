6dd43e666929e617e650027549e0af76dfaa777a
Vanessa Madu <vm2218@ic.ac.uk>
2023-12-12 13:30:15 +0000

    add init and str to Model

--- drifter_velocity_models/model_classes.py
+++ drifter_velocity_models/model_classes.py
@@ -14,3 +14,16 @@ import matplotlib.pyplot as plt
 data = pd.read_hdf("ocean_data.h5")
 
 ####################################### MODEL CLASS ##########################################
+class Model:
+    '''
+    this class will be the parent class of all ocean models that we will be using
+    and it will define attributes and methods common to all specific model classes.
+    '''
+    ## magic methods ##
+    def __init__(self, loss_type,data):
+        self.loss_type = loss_type # specify loss function
+        self.data = data # data as pd dataframe
+    
+    def __str__(self):
+        ''' if a model is operated on by the string operator, it returns a description of the model'''
+        return f"Ocean drifter model with type: {self.model_type}, loss: {self.loss_type}, class: {type(self)}"       
