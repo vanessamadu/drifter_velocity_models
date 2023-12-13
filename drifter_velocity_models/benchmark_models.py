45565f4402b9233e87c144e5f39517346948f571
Vanessa Madu <vm2218@ic.ac.uk>
2023-12-13 12:39:36 +0000

    add sbr

--- drifter_velocity_models/benchmark_models.py
+++ drifter_velocity_models/benchmark_models.py
@@ -4,7 +4,7 @@ import numpy as np
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%% BATHTUB MODEL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
 class BathtubModel(Model):
-    '''this is the first of two benchmark models to measure model performance against'''
+    '''benchmark model - predicts all velocities to be zero at all positions and for all times.'''
     
     #==== magic methods ====#
     def __init__(self, loss_type,training_data,test_data):
@@ -33,3 +33,27 @@ class BathtubModel(Model):
     @property
     def testing_prediction(self):
         return [self.model_function(lon,lat) for lon,lat in np.array(self.training_data[["lon","lat"]])]
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%% SBR MODEL %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+class SBRModel(Model):
+    '''benchmark model: predicts velocities according to a steady solid body rotation model.'''
+
+    #==== magic methods ====#
+    def __init__(self,loss_type:str,training_data,test_data,f0:float):
+        super().__init__(loss_type,training_data,test_data)
+        self.f0 = f0 # coriolis parameter
+        self.model_type = 'sbr'
+    
+    #++++++++++++++++++++++++ STATIC METHODS ++++++++++++++++++++++++++++# 
+    #------------------------ model constructions -------------------------#
+    @staticmethod
+    def sbr(lon,lat,f0):
+        __class__.check_coordinates(lon,lat)
+
+        try:
+            float(f0)
+        except:
+            raise ValueError("coriolis parameter, f0, must be a real number")
+        
+        return np.array([-f0*lat,f0*lon])
