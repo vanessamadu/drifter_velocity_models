4f4145b106de78b8f57f25435c67ef547d0fd9cb
Vanessa Madu <vm2218@ic.ac.uk>
2023-12-14 18:16:00 +0000

    speed adjustments

--- drifter_velocity_models/ng_boosted_model.py
+++ drifter_velocity_models/ng_boosted_model.py
@@ -10,6 +10,9 @@ class NGBoostModel(Model):
         self.model_type = "ngboost_pr"
         self.covariate_labels = covariate_labels
         self.model_function = None
+        self.num_estimators = 10
+        self.trained_dist = None
+        self.test_dist = None
 
     #------------------------ model constructions -------------------------#
     def ngboost_pr(self):
@@ -20,23 +23,23 @@ class NGBoostModel(Model):
         self.model_function.fit(X=np.array(self.training_data.loc[:,self.covariate_labels]),
                                    Y=np.array(self.training_data.loc[:,["u_av","v_av"]]))
     
+    def trained_pred_dist(self):
+        if self.model_function is None:
+            self.ngboost_pr()
+        pred_dist = self.model_function.pred_dist(np.array(self.training_data.loc[:,self.covariate_labels]))
+        self.trained_pred_dist = [pred_dist.loc,pred_dist.cov]
+    
+    def test_pred_dist(self):
+        if self.model_function is None:
+            self.ngboost_pr()
+        pred_dist = self.model_function.pred_dist(np.array(self.test_data.loc[:,self.covariate_labels]))
+        self.test_pred_dist = [pred_dist.loc,pred_dist.cov]
+
     #----------------------- 'immutable' properties -----------------------#
     @property
     def num_estimators(self):
         return self._num_estimators
+    
     @num_estimators.setter
     def num_estimators(self,n):
         self._num_estimators = n
-
-    @property
-    def trained_pred_dist(self):
-        if self.model_function is None:
-            self.ngboost_pr()
-        return self.model_function.pred_dist(np.array(self.training_data.loc[:,self.covariate_labels]))
-
-    @property
-    def test_pred_dist(self):
-        if self.model_function is None:
-            self.ngboost_pr()
-        return self.model_function.pred_dist(np.array(self.test_data.loc[:,self.covariate_labels]))
-    
