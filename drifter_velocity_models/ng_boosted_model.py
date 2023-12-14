3caa9bdcc5fdfe64d87858b0df485df52e9f8e0d
Vanessa Madu <vm2218@ic.ac.uk>
2023-12-14 17:57:59 +0000

    first draft ngboost

--- drifter_velocity_models/ng_boosted_model.py
+++ drifter_velocity_models/ng_boosted_model.py
@@ -9,16 +9,34 @@ class NGBoostModel(Model):
         super().__init__(loss_type,training_data,test_data)
         self.model_type = "ngboost_pr"
         self.covariate_labels = covariate_labels
-        self.fit_model = None
+        self.model_function = None
 
     #------------------------ model constructions -------------------------#
-    def ngboost_pr(self,num_estimators):
+    def ngboost_pr(self):
         '''return probabilistic regression model'''
-        model = ngboost.NGBoost(Dist=ngboost.distns.MultivariateNormal(2),
-                               n_estimators=num_estimators)
+        self.model_function = ngboost.NGBoost(Dist=ngboost.distns.MultivariateNormal(2),
+                               n_estimators=self.num_estimators)
         
-        self.fit_model = model.fit(X=np.array(self.training_data.loc[:,self.covariate_labels]),
+        self.model_function.fit(X=np.array(self.training_data.loc[:,self.covariate_labels]),
                                    Y=np.array(self.training_data.loc[:,["u_av","v_av"]]))
     
-    
+    #----------------------- 'immutable' properties -----------------------#
+    @property
+    def num_estimators(self):
+        return self._num_estimators
+    @num_estimators.setter
+    def num_estimators(self,n):
+        self._num_estimators = n
+
+    @property
+    def trained_pred_dist(self):
+        if self.model_function is None:
+            self.ngboost_pr()
+        return self.model_function.pred_dist(np.array(self.training_data.loc[:,self.covariate_labels]))
+
+    @property
+    def test_pred_dist(self):
+        if self.model_function is None:
+            self.ngboost_pr()
+        return self.model_function.pred_dist(np.array(self.test_data.loc[:,self.covariate_labels]))
     
