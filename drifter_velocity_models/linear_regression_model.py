401f7f215dafb8469a4a84d5c6e14f12ccad6528
Vanessa Madu <vm2218@ic.ac.uk>
2023-12-14 16:55:18 +0000

    parameter estimation

--- drifter_velocity_models/linear_regression_model.py
+++ drifter_velocity_models/linear_regression_model.py
@@ -1,6 +1,7 @@
 from model_classes import Model
 #import packages
 import numpy as np
+from numpy import linalg
 
 class LinearRegressionModel(Model):
     
@@ -8,6 +9,7 @@ class LinearRegressionModel(Model):
         super().__init__(loss_type,training_data,test_data)
         self.model_type = "lr"
         self.covariate_labels = covariate_labels
+        self.param_estimate = None
 
     #------------------------ model constructions -------------------------#
     @staticmethod
@@ -15,7 +17,52 @@ class LinearRegressionModel(Model):
         '''returns a prediction for the linear regression model'''
         try:
             pred = np.matmul(X,beta)
+            return pred
         except:
             ValueError(f"incompatible dimensions covariates : {X.shape}; parameters: {beta.shape}")
-        
+
+    #============== estimate parameter (vector) beta ==================#
+    @property
+    def design(self):
+        '''returns the design matrix associated with the training data'''
+        try:
+            design_matrix = np.array(self.training_data.loc[:,self.covariate_labels])
+            return design_matrix
+        except:
+            raise KeyError("Covariate(s) were not found in the dataset")
+    
+    def calculate_param_estimate(self):
+        '''returns least squares parameter estimate'''
+        lstsq_estimate = linalg.lstsq(self.design,
+                                       np.array(self.training_data.loc[:,["u_av","v_av"]]),
+                                       rcond=None)
+        return lstsq_estimate[0]
+    
+    @property
+    def param_estimate(self):
+        return self._param_estimate
+    
+    @param_estimate.setter
+    def param_estimate(self):
+        if self.param_estimate is not None:
+            return self.param_estimate
+        else:
+            self.param_estimate = self.calculate_param_estimate()
+    #----------------------- 'immutable' properties -----------------------#
+    @property
+    def model_function(self):
+        self.lr
+
+    @property
+    def trained_prediction(self):
+        ' return prediction for each vector of covariates for seen data'
+        pred = self.model_function(self.design,
+                                   self.param_estimate)
         return pred
+    
+    @property
+    def testing_prediction(self):
+        'return prediction for each vector of covariates in test data'
+        pred = self.model_function(np.array(self.test_data.loc[:,self.covariate_labels]),
+                                   self.param_estimate)
+    
